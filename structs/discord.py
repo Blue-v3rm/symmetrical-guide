a818d6ff6277b79a9ced743e3e37b3176d1a0410
Blue-v3rm <61633835+Blue-v3rm@users.noreply.github.com>
2021-01-15 16:00:39 -0500

    Create discord.py

---
+++ structs/discord.py
@@ -0,0 +1,36 @@
+import os, discord, time
+from discord.ext import commands
+from structs import handler
+
+__TOKEN = "" #Your token here.
+
+client = commands.Bot(command_prefix=".")
+channel = None
+
+async def on_register(identifier):
+    print("Registed connection from "+identifier)
+    await channel.send(identifier+" just connected.")
+    pass
+
+async def on_unregister(identifier, *err):
+    print("Unregistered "+identifier)
+    if channel:
+        channel.send(identifier+" closed connection with the websocket.")
+
+@client.event
+async def on_message(message):
+    if message.content[0] != "!": return
+    args = message.content[1:].split(" ")
+    if args[0] == "here":
+        handler.channel = message.channel
+        channel = message.channel
+        await message.channel.send("This channel has been designated to display messages from the websocket.")
+    if len(args) > 0:
+        for v in handler.all_triggers:
+            if v[0] == args[0]:
+                await v[2].send("trigger/"+v[0]+"/"+" ".join(args[1:]))
+    pass
+
+@client.event
+async def on_ready():
+    print('Discord bot ready')
